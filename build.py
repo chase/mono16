832547d665d950f3b26c7f5cb91e2f4376d7b845
Chase Colman <chase@colman.io>
2015-04-09 21:09:53 -0400

    Split build/builder, refactor some more: Needs walker

--- build.py
+++ build.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python2.7
 # vim: sts=4 sw=4 ts=4 et
 
-import fontforge
-from itertools import chain, compress, ifilter, ifilterfalse, imap, product
 from os import mkdir
-from os.path import basename, splitext, join
+from fontbuilder import *
 
 # Configuration
 ## Source directory
@@ -30,82 +28,6 @@ conflicting(
     option('uz', 'Undotted zero', Swap("zero", "zero.dotless")),
 )
 
-# TODO: Implement conflicting option processing
-def conflicting(*args):
-    pass
-
-def style(name, does):
-    if type(does) is not list:
-        does = list(does)
-    option(name, name, [Variant(name)] + does)
-
-    return name
-
-def option(abrv, name, does):
-    operations = option.__dict__.setdefault("operations", {})
-    option.operations[abrv] = does
-
-    return abrv
-
-# Operations
-## NOTE:
-## All operations return a closure with the 1st argument being a fontforge.font
-
-## Adjusts the left and/or right bearings of all glyphs
-def Bearing(left=0, right=0):
-    def op(fnt):
-        fnt.genericGlyphChange(
-                hCounterType="nonUniform",
-                hCounterScale=1.0,
-                lsbScale=1.0,
-                rsbScale=1.0,
-                lsbAdd=left,
-                rsbAdd=right)
-    return op
-
-## Swaps the places of two glyphs using the DEL char as swap space
-def Swap(glyph1, glyph2):
-    def op(fnt):
-        # G1 -> SWP
-        fnt.selection.select(glyph1)
-        fnt.copy()
-        fnt.selection.select(127) # DEL
-        fnt.paste()
-
-        # G2 -> G1
-        fnt.selection.select(glyph2)
-        fnt.copy()
-        fnt.selection.select(glyph1)
-        fnt.paste()
-
-        # SWP -> G1
-        fnt.selection.select(127) # DEL
-        fnt.copy()
-        fnt.selection.select(glyph2)
-        fnt.paste()
-
-        # Clear SWP
-        fnt.selection.select(127) # DEL
-        fnt.clear()
-    return op
-
-## Changes the subfamily/variation of the font
-def Variation(name):
-    def op(fnt):
-        basename = f.fontname.split('-')[0]
-        fontname = [basename] + name.split()
-        f.fontname = '-'.join(fontname)
-        f.fullname = ' '.join(fontname)
-    return op
-
-#############################
-# WARNING: Here be dragons! #
-#############################
-
-# TODO: Rewrite looped-build to streamlined-build
-print("Sorry, not quite done yet")
-exit(1)
-
 # Ensure we have a font release directory
 try:
     mkdir('release')
@@ -113,58 +35,4 @@ except OSError:
     # Already exists, carry on
     pass
 
-# Expand any tuple options into exclusive combinations
-## Split options into tuples and non-tuples
-## Flatten the operation map
-tuples = []
-nontuples = []
-op_map = {}
-for option in options:
-    if type(option) is tuple:
-        tuples.append(option)
-    else:
-        nontuples.append(option)
-
-## Get tuple combinations using the cartesian product of each tuple
-cmbs = product(*nontuples)
-
-## For each combination, append it to the end of the nontuples
-map(lambda cmb: nontuples + list(cmb) , cmbs)
-
-def build(font, opts):
-    # Fork the original font
-    fnt = fontforge.open(join(source, font))
-
-    # Get the base name for the font
-    name = join('release', splitext(basename(font))[0])
-
-    for opt in opts:
-        # Append this option to the font name
-        name += '-' + str(opt)
-
-        # Run all the operations for this option
-        #for oper in options[opt]:
-            #opers[oper](fnt, *oper[opt])
-
-    # Output the file and cleanup
-    fnt.generate(name + ".ttf")
-    fnt.close()
-
-# Get the names/tuples of all the options in alphabetical order
-opt_keys = options.keys()
-opt_len = len(opt_keys)
-
-# Each option is a binary choice, so we use an int as a quick bitmap.
-# To iterate over every possible combination, all we have to do is increment
-# up to the maximum value 2^(#options)
-bitmap_max = 1 << opt_len
-
-# Iterate over all possible combinations
-for i in xrange(bitmap_max):
-    # For each font listed
-    for font in fonts:
-        bitmap = map(lambda n: i >> n & 1, xrange(opt_len))
-        # Build the combinations based on the current bitmap
-        cmbs = list(expand_options(compress(opt_keys, bitmap)))
-        for cmb in cmbs:
-            build(font, cmb)
+# TODO: BUILD!
---
+++ fontbuilder.py
@@ -0,0 +1,129 @@
+# vim: sts=4 sw=4 ts=4 et
+
+import fontforge
+from itertools import chain, compress, ifilter, ifilterfalse, imap
+from os.path import basename, splitext, join
+
+# Builder
+def style(name, does):
+    try:
+        does = list(does)
+    except TypeError:
+        does = [does]
+    if type(does) is not list:
+        does = list(does)
+    option(name, name, [Variation(name)] + does)
+
+    return name
+
+def option(abrv, name, does):
+    option.operations[abrv] = does
+
+    option.count += 1
+    option.unconflicting.add(abrv)
+
+    return abrv
+
+# Initialize the operations map and count
+option.operations = {}
+option.count = 0
+option.unconflicting = set()
+
+def conflicting(*args):
+    conflicting.optsets = [ [arg] + optset for optset in optsets for arg in args ]
+
+    # Only one of the conflicting options should be counted
+    option.count -= len(args) - 1
+    # Deconflict
+    option.unconflicting.difference_update(set(args))
+
+# Initialize the conflicting options set list
+conflicting.optsets = [[]]
+
+# Walk through all the options
+def walk(walker):
+    # Each option is a binary choice, so we use an int as a quick bitmap.
+    # To iterate over every possible combination, all we have to do is increment
+    # up to the maximum value 2^(#options)
+    bitmap_max = 1 << option.count
+
+    # Iterate over all possible combinations
+    for i in xrange(bitmap_max):
+        # For each font listed
+        for font in fonts:
+            bitmap = imap(lambda n: i >> n & 1, xrange(option_count))
+## TODO: Acutally walk the options
+            # Build the combinations based on the current bitmap
+            cmbs = list(expand_options(compress(opt_keys, bitmap)))
+            for cmb in cmbs:
+                walker(cmb)
+
+def build(outdir, font):
+    # Fork the original font
+    fnt = fontforge.open(join(source, font))
+
+    # Get the base name for the font
+    name = join(outdir, splitext(basename(font))[0])
+
+    for opt in opts:
+        # Append this option to the font name
+        name += '-' + str(opt)
+
+        # Run all the operations for this option
+        #for oper in options[opt]:
+            #opers[oper](fnt, *oper[opt])
+
+    # Output the file and cleanup
+    fnt.generate(name + ".ttf")
+    fnt.close()
+
+# Operations
+## NOTE:
+## All operations return a closure with the 1st argument being a fontforge.font
+
+## Adjusts the left and/or right bearings of all glyphs
+def Bearing(left=0, right=0):
+    def op(fnt):
+        fnt.genericGlyphChange(
+                hCounterType="nonUniform",
+                hCounterScale=1.0,
+                lsbScale=1.0,
+                rsbScale=1.0,
+                lsbAdd=left,
+                rsbAdd=right)
+    return op
+
+## Swaps the places of two glyphs using the DEL char as swap space
+def Swap(glyph1, glyph2):
+    def op(fnt):
+        # G1 -> SWP
+        fnt.selection.select(glyph1)
+        fnt.copy()
+        fnt.selection.select(127) # DEL
+        fnt.paste()
+
+        # G2 -> G1
+        fnt.selection.select(glyph2)
+        fnt.copy()
+        fnt.selection.select(glyph1)
+        fnt.paste()
+
+        # SWP -> G1
+        fnt.selection.select(127) # DEL
+        fnt.copy()
+        fnt.selection.select(glyph2)
+        fnt.paste()
+
+        # Clear SWP
+        fnt.selection.select(127) # DEL
+        fnt.clear()
+    return op
+
+## Changes the subfamily/variation of the font
+def Variation(name):
+    def op(fnt):
+        basename = f.fontname.split('-')[0]
+        fontname = [basename] + name.split()
+        f.fontname = '-'.join(fontname)
+        f.fullname = ' '.join(fontname)
+    return op
